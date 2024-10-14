0f906c583c124ef9148dce9381651a3dc985abad
Cedric Verstraeten <cedric@verstraeten.io>
2024-10-14 19:22:12 +0200

    add search field

--- ui/src/features/menu/Menu.jsx
+++ ui/src/features/menu/Menu.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { getMenu } from '../../services/apiRestaurant';
 import MenuItem from './MenuItem';
@@ -5,12 +6,29 @@ import MenuItem from './MenuItem';
 function Menu() {
   const menu = useLoaderData();
 
+  const [query, setQuery] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <ul className="divide-y divide-stone-200 px-2">
-      {menu.map((pizza) => (
-        <MenuItem pizza={pizza} key={pizza.id} />
-      ))}
-    </ul>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Search pizza"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="bg-white-100 my-10 w-full rounded-full px-8 py-3 text-[11px] transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-full sm:focus:w-full"
+        ></input>
+      </form>
+
+      <ul className="divide-y divide-stone-200 px-2">
+        {menu.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
