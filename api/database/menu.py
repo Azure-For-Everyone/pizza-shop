c9b278b52ec2263a72e615740a34dec67bd97796
Cedric Verstraeten <cedric@verstraeten.io>
2024-10-14 18:39:11 +0200

    move pizzas into directory

---
+++ api/database/menu.py
@@ -0,0 +1,243 @@
+menu = [
+    {
+        "id": 1,
+        "name": "Margherita",
+                "unitPrice": 12,
+                "imageUrl": "http://localhost:8889/api/image/pizza-1.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "basil"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 2,
+        "name": "Capricciosa",
+                "unitPrice": 14,
+                "imageUrl": "http://localhost:8889/api/image/pizza-2.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzrarella",
+                    "ham",
+                    "mushrooms",
+                    "artichoke"
+                ],
+        "soldOut": True
+    },
+    {
+        "id": 3,
+        "name": "Romana",
+                "unitPrice": 15,
+                "imageUrl": "http://localhost:8889/api/image/pizza-3.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "prosciutto"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 4,
+        "name": "Prosciutto e Rucola",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-4.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "prosciutto",
+                    "arugula"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 5,
+        "name": "Diavola",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-5.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "spicy salami",
+                    "chili flakes"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 6,
+        "name": "Vegetale",
+                "unitPrice": 13,
+                "imageUrl": "http://localhost:8889/api/image/pizza-6.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "bell peppers",
+                    "onions",
+                    "mushrooms"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 7,
+        "name": "Napoli",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-7.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "fresh tomato",
+                    "basil"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 8,
+        "name": "Siciliana",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-8.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "anchovies",
+                    "olives",
+                    "capers"
+                ],
+        "soldOut": True
+    },
+    {
+        "id": 9,
+        "name": "Pepperoni",
+                "unitPrice": 14,
+                "imageUrl": "http://localhost:8889/api/image/pizza-9.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "pepperoni"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 10,
+        "name": "Hawaiian",
+                "unitPrice": 15,
+                "imageUrl": "http://localhost:8889/api/image/pizza-10.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "pineapple",
+                    "ham"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 11,
+        "name": "Spinach and Mushroom",
+                "unitPrice": 15,
+                "imageUrl": "http://localhost:8889/api/image/pizza-11.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "spinach",
+                    "mushrooms"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 12,
+        "name": "Mediterranean",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-12.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "sun-dried tomatoes",
+                    "olives",
+                    "artichoke"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 13,
+        "name": "Greek",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-13.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "spinach",
+                    "feta",
+                    "olives",
+                    "pepperoncini"
+                ],
+        "soldOut": True
+    },
+    {
+        "id": 14,
+        "name": "Abruzzese",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-14.jpg",
+                "ingredients": [
+                    "tomato",
+                    "mozzarella",
+                    "prosciutto",
+                    "arugula"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 15,
+        "name": "Pesto Chicken",
+                "unitPrice": 16,
+                "imageUrl": "http://localhost:8889/api/image/pizza-15.jpg",
+                "ingredients": [
+                    "pesto",
+                    "mozzarella",
+                    "chicken",
+                    "sun-dried tomatoes",
+                    "spinach"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 16,
+        "name": "Eggplant Parmesan",
+                "unitPrice": 15,
+                "imageUrl": "http://localhost:8889/api/image/pizza-16.jpg",
+                "ingredients": [
+                    "marinara",
+                    "mozzarella",
+                    "eggplant",
+                    "parmesan"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 17,
+        "name": "Roasted Veggie",
+                "unitPrice": 15,
+                "imageUrl": "http://localhost:8889/api/image/pizza-17.jpg",
+                "ingredients": [
+                    "marinara",
+                    "mozzarella",
+                    "zucchini",
+                    "eggplant",
+                    "peppers",
+                    "onions"
+                ],
+        "soldOut": False
+    },
+    {
+        "id": 18,
+        "name": "Tofu and Mushroom",
+                "unitPrice": 15,
+                "imageUrl": "http://localhost:8889/api/image/pizza-18.jpg",
+                "ingredients": [
+                    "marinara",
+                    "mozzarella",
+                    "tofu",
+                    "mushrooms",
+                    "bell peppers"
+                ],
+        "soldOut": False
+    }
+]
--- api/main.py
+++ api/main.py
@@ -1,266 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+from database.menu import menu
 
 app = Flask(__name__)
+CORS(app)
 
-# Sample data
-menu = [
-    {"id": 1, "item": "Pizza", "price": 10.99},
-    {"id": 2, "item": "Burger", "price": 8.99},
-    {"id": 3, "item": "Pasta", "price": 12.99}
-]
-
+# Order Database
 orders = []
 
 
 @app.route('/api/menu', methods=['GET'])
 def get_menu():
-    menu = {
+    payload = {
         "status": "success",
-        "data": [
-            {
-                "id": 1,
-                "name": "Margherita",
-                "unitPrice": 12,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-1.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "basil"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 2,
-                "name": "Capricciosa",
-                "unitPrice": 14,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-2.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "ham",
-                    "mushrooms",
-                    "artichoke"
-                ],
-                "soldOut": True
-            },
-            {
-                "id": 3,
-                "name": "Romana",
-                "unitPrice": 15,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-3.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "prosciutto"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 4,
-                "name": "Prosciutto e Rucola",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-4.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "prosciutto",
-                    "arugula"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 5,
-                "name": "Diavola",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-5.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "spicy salami",
-                    "chili flakes"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 6,
-                "name": "Vegetale",
-                "unitPrice": 13,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-6.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "bell peppers",
-                    "onions",
-                    "mushrooms"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 7,
-                "name": "Napoli",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-7.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "fresh tomato",
-                    "basil"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 8,
-                "name": "Siciliana",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-8.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "anchovies",
-                    "olives",
-                    "capers"
-                ],
-                "soldOut": True
-            },
-            {
-                "id": 9,
-                "name": "Pepperoni",
-                "unitPrice": 14,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-9.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "pepperoni"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 10,
-                "name": "Hawaiian",
-                "unitPrice": 15,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-10.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "pineapple",
-                    "ham"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 11,
-                "name": "Spinach and Mushroom",
-                "unitPrice": 15,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-11.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "spinach",
-                    "mushrooms"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 12,
-                "name": "Mediterranean",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-12.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "sun-dried tomatoes",
-                    "olives",
-                    "artichoke"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 13,
-                "name": "Greek",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-13.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "spinach",
-                    "feta",
-                    "olives",
-                    "pepperoncini"
-                ],
-                "soldOut": True
-            },
-            {
-                "id": 14,
-                "name": "Abruzzese",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-14.jpg",
-                "ingredients": [
-                    "tomato",
-                    "mozzarella",
-                    "prosciutto",
-                    "arugula"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 15,
-                "name": "Pesto Chicken",
-                "unitPrice": 16,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-15.jpg",
-                "ingredients": [
-                    "pesto",
-                    "mozzarella",
-                    "chicken",
-                    "sun-dried tomatoes",
-                    "spinach"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 16,
-                "name": "Eggplant Parmesan",
-                "unitPrice": 15,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-16.jpg",
-                "ingredients": [
-                    "marinara",
-                    "mozzarella",
-                    "eggplant",
-                    "parmesan"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 17,
-                "name": "Roasted Veggie",
-                "unitPrice": 15,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-17.jpg",
-                "ingredients": [
-                    "marinara",
-                    "mozzarella",
-                    "zucchini",
-                    "eggplant",
-                    "peppers",
-                    "onions"
-                ],
-                "soldOut": False
-            },
-            {
-                "id": 18,
-                "name": "Tofu and Mushroom",
-                "unitPrice": 15,
-                "imageUrl": "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/pizzas/pizza-18.jpg",
-                "ingredients": [
-                    "marinara",
-                    "mozzarella",
-                    "tofu",
-                    "mushrooms",
-                    "bell peppers"
-                ],
-                "soldOut": False
-            }
-        ]
+        "data": menu
     }
-    return jsonify(menu)
+    return jsonify(payload)
 
 
 @app.route('/api/order/<int:id>', methods=['GET'])
@@ -292,5 +47,10 @@ def update_order():
         return jsonify({"error": "Order not found"}), 404
 
 
+@app.route('/api/image/<string:id>', methods=['GET'])
+def get_image(id):
+    return send_from_directory('database/images', id)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8889)
